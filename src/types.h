d2fb3af838ce15e7a48b932a6fff2036b20f6d25
Peng LEI <peng.lei@n-hop.com>
2022-10-26 23:01:01 +0800

    feat: define basic types

---
+++ src/types.h
@@ -0,0 +1,19 @@
+/**
+ * @file types.h
+ * @author peng lei (plhitsz@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-10-26
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#ifndef SRC_TYPES_H_
+#define SRC_TYPES_H_
+#include <cstdint>
+#include <vector>
+
+using octetVec = std::vector<uint8_t>;
+using octet = uint8_t;
+
+#endif  // SRC_TYPES_H_
