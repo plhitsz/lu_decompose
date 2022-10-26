c29c3af66e932e87ffefc8a00beea85aea7788f6
Peng LEI <peng.lei@n-hop.com>
2022-10-26 23:28:52 +0800

    feat: impl of lu decomposition

--- src/lu.cc
+++ src/lu.cc
@@ -10,10 +10,8 @@
  */
 #include "lu.h"
 
-#include "field.h"
-#include "matrix.h"
-#include "swapper_matrix.h"
-
+#include <algorithm>
+namespace math {
 int OctetFiniteField_test(int a, int b) {
   OctetFiniteField ff;
   ff.add(1, 1);
@@ -27,3 +25,71 @@ int OctetFiniteField_test(int a, int b) {
   SwapperOctetMatrix test_mat;
   return 0;
 }
+
+template <typename T>
+std::tuple<MatrixOctet, SwapperOctetMatrix, Permutation> LuDecomposition(
+    const MatrixOctet& mat, const AlgebraicField<T>& af) {
+  auto u = SwapperMatrix(mat);
+  // std::cout << "LuDecomposition input mat " << mat;
+  auto l = MatrixOctet(u.Height(), u.Height());
+
+  int irow = 0;
+  int icol = 0;
+  while (irow < u.Height() && icol < u.Width()) {
+    // find pivot and swap it on top
+    for (int jrow : Range(irow, u.Height())) {
+      // pivot found at location (jrow, icol).
+      if (u[jrow][icol] != 0x00) {
+        if (irow != jrow) {
+          u.swapRows(irow, jrow);
+          // diagonal of l is not loaded yet, swapping two rows doesn't affect
+          // its diagonal
+          l.SwapRows(irow, jrow);
+        }
+        break;
+      }
+    }
+
+    // pivot moved to location (irow, icol)
+    if (u[irow][icol] != 0x00) {
+      // eliminating all pivot candidates below
+      for (int jrow : Range(irow + 1, u.Height())) {
+        auto e = af.div(u[jrow][icol], u[irow][icol]);
+
+        // eliminating the `jrow`-th row, starting from the `icol`-th element.
+        // because the previous elements are all zero.
+        for (int jcol : Range(icol, u.Width())) {
+          u[jrow][jcol] = af.sub(u[jrow][jcol], af.mul(u[irow][jcol], e));
+        }
+
+        l[jrow][irow] = e;
+      }
+      // pivot found in (irow, icol), moving to next row.
+      irow++;
+      icol++;
+    } else {
+      // pivot not found in icol
+      icol++;
+    }
+  }
+  auto n = std::min(mat.Height(), mat.Width());
+  // loading l's diagonal
+  for (int i : Range(n)) {
+    l[i][i] = 1;
+  }
+  l.hTruncate(n);
+  l.wTruncate(n);
+  u.wTruncate(n);
+  return {l, u, u.GetPermutation()};
+}
+
+bool isValidLU(const MatrixOctet& l, const SwapperOctetMatrix& u) {
+  for (int i : Range(u.Width())) {
+    if (u[i][i] == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+}  // namespace math
--- src/lu.h
+++ src/lu.h
@@ -11,6 +11,33 @@
 #ifndef SRC_LU_H_
 #define SRC_LU_H_
 
+#include <tuple>
+
+#include "field.h"
+#include "matrix.h"
+#include "swapper_matrix.h"
+
 int OctetFiniteField_test(int a, int b);
 
+namespace math {
+
+using Permutation = std::vector<int>;
+
+template <typename T>
+std::tuple<MatrixOctet, SwapperOctetMatrix, Permutation> luDecomposition(
+    const MatrixOctet& mat, const AlgebraicField<T>& af);
+/**
+ * @brief Check if the LU decomposition is ready to solve. Not to be confused
+ * with checking the sanity of LU. This function ASSUMES LU from a valid lu
+ * decomposition and only checks if the decomposition is ready to solve
+ * (full rank).
+ *
+ * @param l the lower triangular matrix from LU decomposition.
+ * @param u the upper triangular matrix from LU decomposition.
+ * @return true
+ * @return false
+ */
+bool isValidLU(const MatrixOctet& l, const SwapperOctetMatrix& u);
+
+}  // namespace math
 #endif  // SRC_LU_H_
